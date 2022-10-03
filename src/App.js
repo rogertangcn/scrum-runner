4b0ba724b890234d4257b59809411dbc126877a4
Roger Tang <roger.tang@okta.com>
2022-10-02 21:31:37 -0700

    bugfix: client-side routing doesn't work in github hosting

--- src/App.js
+++ src/App.js
@@ -1,16 +1,18 @@
 import * as React from 'react';
 import { QueryClient, QueryClientProvider, useQuery } from '@tanstack/react-query';
-import { createBrowserRouter, RouterProvider, useParams } from "react-router-dom";
+import { createBrowserRouter, RouterProvider, useLocation } from "react-router-dom";
 
 import Page from './component/Page';
-import ErrorPage from "./component/ErrorPage";
+import ErrorPage from './component/ErrorPage';
 import './App.css';
 
 const queryClient = new QueryClient();
 
 function Main() {
-  let { team } = useParams();
-  team = team ?? "access-cert";
+  // NOTE: github react app hosting doesn't support client-side. Have to use query param
+  // https://create-react-app.dev/docs/deployment/#notes-on-client-side-routing
+  const search = useLocation().search;
+  let team = new URLSearchParams(search).get('team') || "access-cert";
 
   const { isLoading, error, data } = useQuery(['appData'], () =>
     fetch('data.json',{
@@ -40,12 +42,7 @@ const router = createBrowserRouter([
     path: "/scrum-runner",
     element: <Main />,
     errorElement: <ErrorPage />,
-  },
-  {
-    path: "/scrum-runner/:team",
-    element: <Main />,
-    errorElement: <ErrorPage />,
-  },
+  }
 ]);
 
 export default function App() {
