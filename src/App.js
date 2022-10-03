4bfefcddd8b5497455b12d1b476678daf76eaa7b
Roger Tang <roger.tang@okta.com>
2022-10-02 20:45:06 -0700

    support ramp/access-req teams

--- src/App.js
+++ src/App.js
@@ -1,13 +1,17 @@
 import * as React from 'react';
 import { QueryClient, QueryClientProvider, useQuery } from '@tanstack/react-query';
+import { createBrowserRouter, RouterProvider, useParams } from "react-router-dom";
 
 import Page from './component/Page';
-
+import ErrorPage from "./component/ErrorPage";
 import './App.css';
 
 const queryClient = new QueryClient();
 
 function Main() {
+  let { team } = useParams();
+  team = team ?? "access-cert";
+
   const { isLoading, error, data } = useQuery(['appData'], () =>
     fetch('data.json',{
       headers : {
@@ -18,20 +22,38 @@ function Main() {
   )
 
   if (isLoading) return 'Loading...'
-
   if (error) return 'An error has occurred: ' + error.message
 
-  const teamMembers = data.filter(user => (user.team || []).includes("access-cert"));
-
+  const teamMembers = data.filter(user => (user.team || []).includes(team));
   return (
     <Page candidates={teamMembers}/>
   );
 }
 
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Main />,
+    errorElement: <ErrorPage />,
+  },
+  {
+    path: "scrum-runner",
+    element: <Main />,
+    errorElement: <ErrorPage />,
+  },
+  {
+    path: "scrum-runner/:team",
+    element: <Main />,
+    errorElement: <ErrorPage />,
+  },
+]);
+
 export default function App() {
   return (
     <QueryClientProvider client={queryClient}>
-      <Main />
+      <React.StrictMode>
+        <RouterProvider router={router} />
+      </React.StrictMode>
     </QueryClientProvider>
   );
 }
